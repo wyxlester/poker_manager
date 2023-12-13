6832150068d6330826366eddfbcf5b3cd44321a0
lester wee <lester.wee.68@gmail.com>
2023-12-14 03:02:42 +0800

    created validated models and migrated. test correct using both python shell and admin

--- poker_host/admin.py
+++ poker_host/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Player, Game, Session, PlayerSession
 
 # Register your models here.
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "bankroll", "user_id")
+    list_editable = ("name", "bankroll", "user_id")
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ("id", "game_type", "small_blind", "big_blind", "user_id")
+    list_editable = ("game_type", "small_blind", "big_blind", "user_id")
+
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ("id", "game_id", "start_time", "end_time")
+    list_editable = ("game_id", "start_time", "end_time")
+
+class PlayerSessionAdmin(admin.ModelAdmin):
+    list_display = ("id", "player_id", "session_id", "buy_in", "cash_out", "notes")
+    list_editable = ("player_id", "session_id", "buy_in", "cash_out", "notes")
+
+admin.site.register(Player, PlayerAdmin)
+admin.site.register(Game, GameAdmin)
+admin.site.register(Session, SessionAdmin)
+admin.site.register(PlayerSession, PlayerSessionAdmin)
--- poker_host/models.py
+++ poker_host/models.py
@@ -1,3 +1,57 @@
+from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    bankroll = models.IntegerField(default=0)
+    user_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    def __str__(self):
+        return f"Name: {self.name}, Bankroll: {self.bankroll}, Belongs to: {self.user_id}"
+
+class Game(models.Model):
+    GAME_FORMATS = [
+        ("NLHE", "No-Limit Hold'em"),
+        ("LHE", "Limit Hold'em"),
+        ("PLO-4", "Pot-Limit Omaha (4 cards)"),
+        ("PLO-5", "Pot-Limit Omaha (5 cards)"),
+        ("PLO-6", "Pot-Limit Omaha (6 cards)"),
+        ("PLO hi/lo-4", "Pot-Limit Omaha Hi/Lo (4 cards)"),
+        ("PLO hi/lo-5", "Pot-Limit Omaha Hi/Lo (5 cards)"),
+        ("Dealer's Choice", "Dealer's Choice"),
+    ]
+    game_type = models.CharField(max_length=100, choices=GAME_FORMATS)
+    small_blind = models.IntegerField(validators=[MinValueValidator(1)])
+    big_blind = models.IntegerField(validators=[MinValueValidator(1)])
+    user_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+
+class Session(models.Model):
+    game_id = models.ForeignKey(
+        Game,
+        on_delete=models.CASCADE,
+    )
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+
+class PlayerSession(models.Model):
+    player_id = models.ForeignKey(
+        Player,
+        on_delete=models.CASCADE,
+    )
+    session_id = models.ForeignKey(
+        Session,
+        on_delete=models.CASCADE,
+    )
+    buy_in = models.IntegerField(validators=[MinValueValidator(1)])
+    cash_out = models.IntegerField(default=0)
+    rebuy = models.IntegerField(default=0)
+    notes = models.TextField(max_length=1000, blank=True, null=True)
